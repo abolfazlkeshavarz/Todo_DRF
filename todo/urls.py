7d4ae3e0c97c7655e896e1a740e558c8e25ede68
Abolfazl Keshavarz <abolfazlkeshavarz1112@gmail.com>
2024-07-18 12:05:20 +0330

    update via mixins

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('<int:todo_id>', views.detailed_view),
     path('cbv/', views.ListAPIView.as_view()),
     path('cbv/<int:todo_id>', views.DetailedListAPIView.as_view()),
+    path('mixins/', views.MixinViews.as_view()),
+    path('mixins/<int:pk>', views.MixinDetailedView.as_view()),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,7 +6,9 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from .models import Todo
 from .serializers import TodoSerializer
-# Create your views here.
+from rest_framework import generics, mixins
+
+#region Function based Views
 @api_view(['GET', 'POST'])
 def all_todos(request: Request):
     if request.method == 'GET':
@@ -19,7 +21,6 @@ def all_todos(request: Request):
             serialize.save()
             return Response(serialize.data, status.HTTP_201_CREATED)
     return Response(None, status.HTTP_400_BAD_REQUEST)
-
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def detailed_view(request : Request, todo_id:int):
@@ -42,6 +43,9 @@ def detailed_view(request : Request, todo_id:int):
     elif request.method == 'DELETE':
         todo.delete()
         return Response(None, status = status.HTTP_204_NO_CONTENT)
+#endregion
+
+#region Class based Views
 
 
 class ListAPIView(APIView):
@@ -82,3 +86,28 @@ class DetailedListAPIView(APIView):
         todo = self.get_object(todo_id)
         todo.delete()
         return Response(None, status = status.HTTP_204_NO_CONTENT)
+
+#endregion
+
+#region mixin Views
+class MixinViews(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Todo.objects.order_by('priority').all()
+    serializer_class = TodoSerializer
+
+    def get(self, request:Request):
+        return self.list(request=request)
+    def post(self, request:Request):
+        return self.create(request=request)
+#endregion
+
+class MixinDetailedView(mixins.DestroyModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+    def get(self, request: Request, pk):
+        return self.retrieve(request, pk)
+    def put(self, request: Request, pk):
+        return self.update(request, pk)
+    def delete(self, request: Request, pk):
+        return self.destroy(request, pk)
